291e171976891863d7e72f287017126a9aef06d6
jframirez161 <johnf.ramirez@udea.edu.co>
2024-08-23 11:35:22 +0200

    first commit

--- src/components/OdeSolver.js
+++ src/components/OdeSolver.js
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
-import { odeSystem } from './odeFunctions'; // Importing the ODE function
+import Button from '@mui/material/Button'; 
+import { Box } from '@mui/material'; 
+import { odeSystem } from './odeFunctions'; 
 
 
-const eulerMethod = (t0, t1, initialConditions, odeSystem, dt) => {
+const RK4 = (t0, t1, initialConditions, odeSystem, dt) => {
     const times = [t0];
     const solutions = [initialConditions];
     let currentTime = t0;
     let currentState = initialConditions;
 
     while (currentTime < t1) {
-        const derivatives = odeSystem(currentTime, currentState);
-        currentState = currentState.map((value, index) => value + dt * derivatives[index]);
+        const k1 = odeSystem(currentTime, currentState);
+        const k2 = odeSystem(
+            currentTime + dt / 2,
+            currentState.map((value, index) => value + dt / 2 * k1[index])
+        );
+        const k3 = odeSystem(
+            currentTime + dt / 2,
+            currentState.map((value, index) => value + dt / 2 * k2[index])
+        );
+        const k4 = odeSystem(
+            currentTime + dt,
+            currentState.map((value, index) => value + dt * k3[index])
+        );
+
+        currentState = currentState.map(
+            (value, index) => value + (dt / 6) * (k1[index] + 2 * k2[index] + 2 * k3[index] + k4[index])
+        );
         currentTime += dt;
 
         times.push(currentTime);
@@ -26,16 +43,148 @@ function OdeSolver() {
         
     const [time, setTime] = useState([]);
     const [solution, setSolution] = useState([]);
+
+    // Define the state variable names
+    const stateVariableNames = [
+        'intake', 'H2O', 'Pi', 'Am', 'Ba', 'Bf', 'Fn', 'PoFl', 'SpFl', 'Tg',
+        'Fu', 'Fd', 'Si', 'Sf', 'La', 'Sa', 'Sfn', 'Lactate', 'Ac', 'Acetate',
+        'Bu', 'Butyrate', 'Pr', 'Propionate', 'Br', 'NOP', 'NO3', 'NO2', 'N2O',
+        'NADH', 'H2', 'Me', 'CH4', 'CO2', 'Bicarb', 'Hplus', 'maxHplus',
+        'InsolFSG', 'OMHind', 'OMFecal', 'CH4_hind', 'fecalH2O', 'fecalN',
+        'fecalC', 'Dp', 'Ip', 'RMp', 'Gp', 'Np', 'LBa', 'LBf', 'Lp', 'LMp',
+        'Tp', 'Ep', 'Fp', 'Up', 'Po', 'Sp', 'Ps', 'Ws', 'FaU', 'FaS'
+    ];
     
-    // State to track selected variables for each plot
-    const max = 62;
-    const sequenceArray = [];
+    const plotsNames1 = [
+        'Fluids and Solutes', 'Feed Components', 'Microorganisms'
+    ];
+    
+    const plotsNames2 = [
+        'Additives', 'Organic Acids', 'Gases'
+    ];
+    
+    const plotsNames3 = [
+        'Other Chemicals', 'Phosphorus', 'Fecal, and Urine Outputs'
+    ];
 
-    for (let i = 0; i <= max; i++) {
-        sequenceArray.push(i);
-    }
+    // Specific lists for each plot
+    const plot1Variables = ['intake', 'H2O'];
+    const plot2Variables = ['Fu', 'Fd', 'Si', 'Ws', 'Pi', 'Ps', 'Am', 'Tg', 'FaU', 'FaS']; 
+    const plot3Variables = ['Me','Ba','Bf','Fn','Po','PoFl','SpFl','Sa','Sf','Sfn', 'Sp','LBa', 'LBf']; 
+    const plot4Variables = ['Br', 'NOP', 'N3O', 'NO2', 'N2O']; 
+    const plot5Variables = ['La', 'Lactate', 'Ac', 'Acetate', 'Bu', 'Butyrate', 'Pr', 'Propionate']; 
+    const plot6Variables = ['CH4', 'CH4_hind', 'fecalH2O', 'H2', 'CO2']; 
+    const plot7Variables = ['NADH', 'Bicarb', 'Hplus', 'InsolFSG']; 
+    const plot8Variables = ['Dp', 'Ip', 'RMp', 'Gp',  'Np',  'Lp',  'LMp', 'Tp', 'Ep']; 
+    const plot9Variables = ['OMHind', 'OMFecal', 'fecalN', 'fecalC', 'Fp', 'Up']; 
+    
+    // Create a dictionary to hold descriptions for each state variable
+const variableDescriptions = {
+    
+    // Fluids and Solutes
+    intake: 'intake (grams)',
+    H2O: 'H2O/water with dissolved solutes (L)',
+    
+    // Feed Components
+    Fu: 'undegradable fiber (mol)',
+    Fd: 'degradable fiber (mol)',
+    Si: 'insoluble [degradable] starch (mol)',
+    Ws: 'water soluble carbohydrates [including glycerol] (mol)',
+    Pi: 'insoluble (degradable) protein (mol)',
+    Ps: 'soluble protein (mol)',
+    Am: 'ammonia (mol)',
+    Tg: 'triacylglycerides (mol)',
+    FaU: 'unsaturated long chain fatty acid (mol)',
+    FaS: 'saturated long chain fatty acid (mol)',
+    
+    // Microorganisms
+    Me: 'methanogens (grams)',
+    Ba: 'amylolytic bacteria (grams)',
+    Bf: 'fibrolytic bacteria (grams)',
+    Fn: 'anaerobic fungi (grams)',
+    Po: 'protozoa (grams)',        
+    PoFl: 'fluid-associated protozoa (grams)',
+    SpFl: 'fluid-associated protozoal storage polysaccharide (grams)',
+    Sa: 'amylolytic bacterial storage polysaccharide (grams)',
+    Sf: 'fibrolytic bacterial storage polysaccharide (grams)',
+    Sfn: 'anaerobic fungi storage polysaccharide (grams)',
+    Sp: 'protozoal storage polysaccharide (grams)',
+    LBa: 'Large intestine amylolytic bacteria (grams)',
+    LBf: 'Large intestine fibrolytic bacteria (grams)',
+    
+    // Additives
+    Br: 'bromoform (mol)',
+    NOP: '3NOP (mol)',
+    N3O: 'nitrate (mol)',
+    NO2: 'nitrite (mol)',
+    N2O: 'nitrous oxide (mol)',
+    
+    // Organic Acids
+    La: 'lactic acid (mol)',
+    Lactate: 'Lactate [disassociated lactic acid] (mol)',
+    Ac: 'acetic acid (mol)',
+    Acetate: 'Acetate [disassociated acetic acid] (mol)',
+    Bu: 'butyric acid (mol)',
+    Butyrate: 'Butyrate [disassociated butyric acid](mol)',
+    Pr: 'propionic acid (mol)',
+    Propionate: 'Propionate [disassociated propionic acid] (mol)',
+    
+    // Gases
+    CH4: 'methane (mol)',
+    CH4_hind: 'CH4 produced from organic matter in hindgut (mol)',
+    fecalH2O: 'fecal water (L)',
+    H2: 'metabolic hydrogen gas',        
+    CO2: 'carbon dioxide',
+    
+    // Other Chemicals
+    NADH: 'reduced NAD+ (mol)',
+    Bicarb: 'bicarbonate (mol)',
+    Hplus: 'hydrogen ion (mol)',
+    InsolFSG: 'ratio of density of particle to density of water',
+    
+    
+    // Phosphorus
+    Dp: 'Rumen digestible phosphorus (grams)',
+    Ip: 'Rumen indigestible phosphorus (grams)',
+    RMp: 'Rumen microbial phosphorus (grams)',
+    Gp: 'Small intestinal digestible phosphorus (grams)',
+    Np: 'Small intestinal indigestible phosphorus (grams)',       
+    Lp: 'Large intestinal digestible phosphorus (grams)',
+    LMp: 'Large intestinal microbial phosphorus (grams)',
+    Tp: 'Large intestinal indigestible phosphorus (grams)',
+    Ep: 'extracellular phosphorus(grams)',
+    
+    // Fecal, and Urine Outputs
+    OMHind: 'organic matter in hindgut (grams)',
+    OMFecal: 'organic matter in feces (grams)',       
+    fecalN: 'manure Nitrogen (mol)',
+    fecalC: 'manure carbon (mol)',
+    Fp: 'fecal phosphorus (grams)',
+    Up: 'urine phosphorus (grams)'
 
-    const [selectedVars, setSelectedVars] = useState([0, 1, 2]);
+};
+
+
+    
+
+    // Set the indices corresponding to the initial values for each plot
+    const [selectedVars1, setSelectedVars1] = useState([
+        stateVariableNames.indexOf('intake'), // Initial default for Plot 1
+        stateVariableNames.indexOf('Fu'),    // Initial default for Plot 2
+        stateVariableNames.indexOf('Me')     // Initial default for Plot 3
+    ]);
+
+    const [selectedVars2, setSelectedVars2] = useState([
+        stateVariableNames.indexOf('Br'), // Initial default for second row Plot 1
+        stateVariableNames.indexOf('La'),    // Initial default for second row Plot 2
+        stateVariableNames.indexOf('CH4')     // Initial default for second row Plot 3
+    ]);
+    
+    const [selectedVars3, setSelectedVars3] = useState([
+        stateVariableNames.indexOf('NADH'), // Initial default for second row Plot 1
+        stateVariableNames.indexOf('Dp'),    // Initial default for second row Plot 2
+        stateVariableNames.indexOf('OMHind')     // Initial default for second row Plot 3
+    ]);
 
     const solveODE = () => {
         // Define your initial conditions as per the model
@@ -103,57 +252,69 @@ function OdeSolver() {
  1e-10,
  1e-10];
 
-        // Solve ODE using numeric.dopri
-        // numeric.dopri(t0, t1, x0, f, tol, maxit, h0)
-        //t0: The initial time.
-        //t1: The final time.
-        //x0: The initial conditions (an array of initial values for the state variables).
-        //f: The function that defines the system of ODEs.
-        //tol (optional): The tolerance for the error estimate (usually a small number like 1e-6 or 1e-8).
-        //maxit (optional): The maximum number of iterations (if you want to limit the iterations).
-        //h0 (optional): The initial step size for the solver (if you want to control the step size manually).
-
-        // Solve ODE using numeric.dopri
-        //let sol = numeric.dopri(0, 24, initialConditions, odeSystem, 1e-6, 1000, 0.01);
-
-        // Process solution to extract each state variable's evolution over time
-        //const times = sol.x;
-        //const solutions = sol.y; 
-
-        //setTime(times);
-        //setSolution(solutions);  
-        
+    
         
         const t0 = 0;
         const t1 = 24;
         const dt = 0.001; // Step size
 
-        const { times, solutions } = eulerMethod(t0, t1, initialConditions, odeSystem, dt);
+        const { times, solutions } = RK4(t0, t1, initialConditions, odeSystem, dt);
 
         setTime(times);
         setSolution(solutions);
 
+        // Reset selectedVars to show default variables in each plot after solving ODE
+        setSelectedVars1([
+            stateVariableNames.indexOf(plot1Variables[0]), // Default for Plot 1
+            stateVariableNames.indexOf(plot2Variables[0]), // Default for Plot 2
+            stateVariableNames.indexOf(plot3Variables[0])  // Default for Plot 3
+        ]);
+
+        setSelectedVars2([
+            stateVariableNames.indexOf(plot4Variables[0]), // Default for second row Plot 1
+            stateVariableNames.indexOf(plot5Variables[0]), // Default for second row Plot 2
+            stateVariableNames.indexOf(plot6Variables[0])  // Default for second row Plot 3
+        ]);
+        
+        setSelectedVars3([
+            stateVariableNames.indexOf(plot7Variables[0]), // Default for second row Plot 1
+            stateVariableNames.indexOf(plot8Variables[0]), // Default for second row Plot 2
+            stateVariableNames.indexOf(plot9Variables[0])  // Default for second row Plot 3
+        ]);
+
         console.log(times); 
-
-         
-
     };
 
-    // Handler to update selected state variable for each dropdown
-    const handleSelectionChange = (index, event) => {
-        const newSelectedVars = [...selectedVars];
-        newSelectedVars[index] = event.target.value;
-        setSelectedVars(newSelectedVars);
+    // Handler to update selected state variable for each dropdown (first row of plots)
+    const handleSelectionChange1 = (index, event) => {
+        const newSelectedVars1 = [...selectedVars1];
+        newSelectedVars1[index] = event.target.value;
+        setSelectedVars1(newSelectedVars1);
+    };
+
+    // Handler to update selected state variable for each dropdown (second row of plots)
+    const handleSelectionChange2 = (index, event) => {
+        const newSelectedVars2 = [...selectedVars2];
+        newSelectedVars2[index] = event.target.value;
+        setSelectedVars2(newSelectedVars2);
+    };
+    
+    // Handler to update selected state variable for each dropdown (second row of plots)
+    const handleSelectionChange3 = (index, event) => {
+        const newSelectedVars3 = [...selectedVars3];
+        newSelectedVars3[index] = event.target.value;
+        setSelectedVars3(newSelectedVars3);
     };
 
     // Prepare data for each plot based on the selected variable
     const getPlotData = (varIndex) => ({
         labels: time.map(t => Math.round(t)),
         datasets: [{
-            label: `State Variable ${parseInt(varIndex) + 1}`,
+            label: stateVariableNames[varIndex], // Use state variable names here
             data: solution.map(sol => sol[varIndex]),  // Extract the selected state variable over time
             fill: false,
-            borderColor: `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+            borderColor: 'rgb(0, 0, 0)', //`rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`,
+            borderWidth: 1, 
             tension: 0.1
         }]
     });
@@ -164,40 +325,119 @@ function OdeSolver() {
             x: {
                 title: {
                     display: true,
-                    text: 'Time (units)',  // Replace 'Time (units)' with your preferred label
+                    text: 'Time (Hours)',  // Replace 'Time (units)' with your preferred label
                 },
             },
             y: {
                 title: {
                     display: true,
-                    text: `State Variable ${parseInt(varIndex) + 1}`,  // Replace with your preferred y-axis label
+                    text: stateVariableNames[varIndex],  // Use state variable names here
                 },
             },
         },
     });
 
+    
+    const plotContainerStyle = {
+        display: 'flex',
+        justifyContent: 'space-around', // This spreads out the children more evenly
+        marginBottom: '20px' // Adds vertical space between rows of plots
+    };
+    
+    const labelStyle = {
+        fontWeight: 'bold',       // Makes text bold
+        color: '#007BFF',         // Sets text color, choose a color that fits your app's theme
+        backgroundColor: '#E8F0FE', // Light blue background, change as needed
+        padding: '5px 10px',     // Adds padding around the text
+        borderRadius: '5px',     // Rounds the corners of the background
+        display: 'inline-block', // Ensures padding and background enclose the text
+        marginBottom: '10px'     // Adds space below the label
+    };
+
     return (
         <div>
-            <button onClick={solveODE}>Solve ODE</button>
-            
-            <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px' }}>
-                {selectedVars.map((selectedVar, index) => (
-                    <div key={index} style={{ width: '30%' }}>
-                        <label>Select Variable for Plot {index + 1}: </label>
-                        <select 
-                            value={selectedVar} 
-                            onChange={(e) => handleSelectionChange(index, e)}
-                            style={{ marginBottom: '10px', display: 'block' }}
-                        >
-                            {solution.length > 0 && solution[0].map((_, i) => (
-                                <option key={i} value={i}>
-                                    State Variable {i + 1}
-                                </option>
-                            ))}
-                        </select>
-                        <Line data={getPlotData(selectedVar)} options={getPlotOptions(selectedVar)} />
-                    </div>
-                ))}
+            <Box textAlign="center">
+                <Button variant="contained" color="primary" onClick={solveODE}>
+                    Solve ODE
+                </Button>
+            </Box>
+
+            {/* First row of plots */}
+            <div style={plotContainerStyle}>
+                {selectedVars1.map((selectedVar, index) => {
+                    const optionsList = index === 0 ? plot1Variables
+                                    : index === 1 ? plot2Variables
+                                    : plot3Variables;
+                    return (
+                        <div key={index} style={{ width: '30%' }}>
+                            <label style={labelStyle}>{plotsNames1[index]}: </label>
+                            <select 
+                                value={selectedVar} 
+                                onChange={(e) => handleSelectionChange1(index, e)}
+                                style={{ marginBottom: '10px', display: 'block' }}
+                            >
+                                {optionsList.map((variable, i) => (
+                                    <option key={i} value={stateVariableNames.indexOf(variable)}>
+                                    {variable + ' - ' + (variableDescriptions[variable] || 'No description available')}
+                                    </option>
+                                ))}
+                            </select>
+                            <Line data={getPlotData(selectedVar)} options={getPlotOptions(selectedVar)} />
+                        </div>
+                    );
+                })}
+            </div>
+
+            {/* Second row of plots */}
+            <div style={plotContainerStyle}>
+                {selectedVars2.map((selectedVar, index) => {
+                    const optionsList = index === 0 ? plot4Variables
+                                    : index === 1 ? plot5Variables
+                                    : plot6Variables;
+                    return (
+                        <div key={index} style={{ width: '30%' }}>
+                            <label style={labelStyle}>{plotsNames2[index]}: </label>
+                            <select 
+                                value={selectedVar} 
+                                onChange={(e) => handleSelectionChange2(index, e)}
+                                style={{ marginBottom: '10px', display: 'block' }}
+                            >
+                                {optionsList.map((variable, i) => (
+                                    <option key={i} value={stateVariableNames.indexOf(variable)}>
+                                        {variable + ' - ' + (variableDescriptions[variable] || 'No description available')}
+                                    </option>
+                                ))}
+                            </select>
+                            <Line data={getPlotData(selectedVar)} options={getPlotOptions(selectedVar)} />
+                        </div>
+                    );
+                })}
+            </div>
+
+            {/* Third row of plots */}
+            <div style={plotContainerStyle}>
+                {selectedVars3.map((selectedVar, index) => {
+                    const optionsList = index === 0 ? plot7Variables
+                                    : index === 1 ? plot8Variables
+                                    : plot9Variables;
+                    return (
+                        <div key={index} style={{ width: '30%' }}>
+                            <label style={labelStyle}>{plotsNames3[index]}: </label>
+                            <select 
+                                value={selectedVar} 
+                                onChange={(e) => handleSelectionChange3(index, e)}
+                                style={{ marginBottom: '10px', display: 'block' }}
+                            >
+                                {optionsList.map((variable, i) => (
+                                    <option key={i} value={stateVariableNames.indexOf(variable)}>
+                                        {variable + ' - ' + (variableDescriptions[variable] || 'No description available')}
+                                    </option>
+                                ))}
+                            </select>
+                            <Line data={getPlotData(selectedVar)} options={getPlotOptions(selectedVar)} />
+                        </div>
+                    );
+                })}
             </div>
         </div>
     );
